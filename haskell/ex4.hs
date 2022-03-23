f1e29db8bcf549e23183bf538a692955c7607fbe
Anders laptop <99sund@gmail.com>
2022-03-23 14:02:16 +0100

    ny time

--- haskell/ex4.hs
+++ haskell/ex4.hs
@@ -40,3 +40,82 @@ averageGrade s = wsum / (fromIntegral $ length cs)
         cs = courses s
         wsum = sum $ [fromInteger (fromJust (grade c)) * weight c | c <- cs, isJust (grade c)]
 
+-- Lights out --
+data Lights = Lights [Bool] deriving Show
+
+toggle :: Lights -> Int -> Lights
+toggle (Lights ls) i = Lights $ left ++ map not mid ++ right
+    where  
+        (left, rest) = splitAt (i-1) ls
+        (mid, right) = splitAt (if i == 0 then 2 else 3) rest
+
+
+-- Tic-tac-toe --
+data Player = Cross | Nought
+data Board = Board [[Maybe Player]]
+
+
+-- CharLike --
+class CharLike a where
+    convert :: a -> Char
+    similar :: CharLike b => a -> b -> Bool
+    similar x y = convert x == convert y
+
+--instance CharLike String where
+--    convert = head
+
+instance CharLike Char where
+    convert = id
+
+instance CharLike Bool where
+    convert True = 'T'
+    convert False = 'F'
+
+instance CharLike Integer where
+    convert = head . show
+
+class Game g where
+    isEnd :: g -> Bool
+
+instance Game Lights where
+    isEnd (Lights ls) = and ls
+
+class PlayerSet s where
+    players :: s a -> [a]
+
+data PlayerList a = PlayerList [a]
+
+instance PlayerSet PlayerList where
+    players (PlayerList xs) = xs
+
+data PlayerTuple a = PlayerTuple (a,a,a)
+
+instance PlayerSet PlayerTuple where
+    players (PlayerTuple (x, y, z)) = [x, y, z]
+
+data SinglePlayer a = SinglePlayer a
+
+instance PlayerSet SinglePlayer where
+    players (SinglePlayer p) = [p]
+
+-- Functors --
+data MyMaybe a = MyJust a | MyNothing
+
+mysucc :: Integer -> Integer
+mysucc = succ
+
+myint :: MyMaybe Integer
+myint = MyJust 10
+
+instance Functor MyMaybe where
+    fmap f (MyJust x) = MyJust $ f x
+    fmap f MyNothing = MyNothing
+
+data Box a = Box a 
+
+instance Functor Box where
+    fmap f (Box x) = Box $ f x
+
+instance Functor PlayerList where
+    fmap f (PlayerList xs) = PlayerList $ map f xs
+
